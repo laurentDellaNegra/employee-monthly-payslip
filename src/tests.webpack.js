c91e366ca7a0f612330581df71c4b3de744394d6
LaurentDellaNegra <laurent.dellanegra@gmail.com>
2016-07-31 21:43:37 +1000

    fc

--- src/tests.webpack.js
+++
@@ -1,7 +0,0 @@
-// This file is an entry point for angular tests
-
-import 'angular';
-import 'angular-mocks/angular-mocks';
-
-var testsContext = require.context("./app", true, /.spec\.js$/);
-testsContext.keys().forEach(testsContext);
